6e846ea31dfaeb0bf1cec710315b4d35e160a468
Rohan Tondelkar <rohanvaibhavtondlekar@gmail.com>
2026-06-11 15:00:25 +0200

    feat: export User and other models from models package

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+# Import models to register them with metadata
+from models.user import User  # noqa: F401
+from models.body import BodyMetric  # noqa: F401
+from models.nutrition import MealLog  # noqa: F401
+from models.workout import WorkoutLog, ExerciseSet  # noqa: F401
